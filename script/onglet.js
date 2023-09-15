13b592143ccf60fe986c325832d1042c92566376
Alexis-Contente <alexis.contente@gmail.com>
2023-09-15 22:01:45 +0200

    add videos and js

--- script/onglet.js
+++ script/onglet.js
@@ -20,6 +20,18 @@ onglets.forEach((onglet) => {
   });
 });
 
-document.getElementById("downloadBtn").addEventListener("click", function () {
-  (window.location.href = "/assets/Alexis CONTENTE.pdf"), "_blank";
+document.addEventListener("DOMContentLoaded", function () {
+  const videoContainers = document.querySelectorAll(".video-container");
+
+  videoContainers.forEach(function (container) {
+    const video = container.querySelector("video");
+
+    container.addEventListener("mouseenter", function () {
+      video.play();
+    });
+
+    container.addEventListener("mouseleave", function () {
+      video.pause();
+    });
+  });
 });
